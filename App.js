74d2362fa2141cff1d15e072346d75457157bb15
shahzaibali2122 <shahzaibkarim74@gmail.com>
2023-12-11 18:16:24 +0500

    Remove heading

--- App.js
+++ App.js
@@ -19,7 +19,7 @@ export default class App extends React.Component {
             data={{dataSets:[{label: "demo", values: [{y: 1}, {y: 2}, {y: 1}]}]}}
           />
         </View>
-        <Text>Hello World</Text>
+        
       </View>
     );
   }
