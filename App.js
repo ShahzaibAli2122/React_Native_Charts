0b21de18c7d111a221a6b544d3d5c2e8849aae3a
shahzaibali2122 <shahzaibkarim74@gmail.com>
2024-02-01 03:42:25 +0500

    charts

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View, processColor
+} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import {LineChart} from 'react-native-charts-wrapper';
+
+export default class App extends React.Component {
+
+  render() {
+    return (
+      <View style={{flex: 1}}>
+        <View style={styles.container}>
+          <LineChart style={styles.chart}
+            data={{dataSets:[{label: "demo", values: [{y: 1}, {y: 2}, {y: 1}]}]}}
+          />
+        </View>
+        <Text style={{fontSize:30,backgroundColor:'red'}}>Hello</Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#F5FCFF'
   },
+  chart: {
+    flex: 1
+  }
 });
