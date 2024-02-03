9efd646e11f14e04cc7fda0b9658fde6db6d5a7e
shahzaibali2122 <shahzaibkarim74@gmail.com>
2024-02-03 17:38:07 +0500

    Fix errors

--- App.js
+++ App.js
@@ -18,26 +18,7 @@ export default class App extends React.Component {
             data={{dataSets:[{label: "demo", values: [{y: 1}, {y: 2}, {y: 1}]}]}}
           />
         </View>
-        <Text style={{fontSize:30,backgroundColor:'red'}}>Hello</Text>
-        <Text style={{fontSize:30,backgroundColor:'red'}}>Hello</Text>
-        <Text style={{fontSize:30,backgroundColor:'red'}}>Hello</Text>
-        <Text style={{fontSize:30,backgroundColor:'red'}}>Hello</Text>
-        <Text style={{fontSize:30,backgroundColor:'red'}}>Hello</Text>
-        <Text style={{fontSize:30,backgroundColor:'red'}}>Hello</Text>
-        <Text style={{fontSize:30,backgroundColor:'red'}}>Hello</Text>
-        <Text style={{fontSize:30,backgroundColor:'red'}}>Hello</Text>
-        <Text style={{fontSize:30,backgroundColor:'red'}}>Hello</Text>
-        <Text style={{fontSize:30,backgroundColor:'red'}}>Hello</Text>
-        <Text style={{fontSize:30,backgroundColor:'red'}}>Hello</Text>
-        <Text style={{fontSize:30,backgroundColor:'red'}}>Hello</Text>
-        <Text style={{fontSize:30,backgroundColor:'red'}}>Hello</Text>
-        <Text style={{fontSize:30,backgroundColor:'red'}}>Hello</Text>
-        <Text style={{fontSize:30,backgroundColor:'red'}}>Hello</Text>
-        <Text style={{fontSize:30,backgroundColor:'red'}}>Hello</Text>
-        <Text style={{fontSize:30,backgroundColor:'red'}}>Hello</Text>
-        <Text style={{fontSize:30,backgroundColor:'red'}}>Hello</Text>
-        <Text style={{fontSize:30,backgroundColor:'red'}}>Hello</Text>
-        <Text style={{fontSize:30,backgroundColor:'red'}}>Hello</Text>
+        
       </View>
     );
   }
