791c712030d406f1a64e127330d11028bdc92ddc
shahzaibali2122 <shahzaibkarim74@gmail.com>
2024-02-07 16:31:51 +0500

    Bar chart

---
+++ Charts/BarChart.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const BarChart = () => {
+  return (
+    <View>
+      <Text>BarChart</Text>
+    </View>
+  )
+}
+
+export default BarChart
