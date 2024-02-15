de68a856065f04c3f2cb67edec6549e3eedd4a9e
shahzaibali2122 <shahzaibkarim74@gmail.com>
2024-02-16 01:11:48 +0500

    Sizing

--- App.js
+++ App.js
@@ -13,7 +13,7 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={{flex: 1}}>
-        <Text style={{}} >Line Chart</Text>
+        <Text style={{fontSize:20}} >Line Chart</Text>
         <View style={styles.container}>
           <LineChart style={styles.chart}
             data={{dataSets:[{label: "demo", values: [{y: 1}, {y: 2}, {y: 1}]}]}}
